a0899b2d1c0e244deaf1ba16fab91c60489fe0eb
froliveira1985 <froliveira1985@gmail.com>
2022-06-15 09:17:49 -0300

    Funções-Coleções-Map-Filter-Reduce

---
+++ JavaScript/Funcoes/alunosAprovados.js
@@ -0,0 +1,38 @@
+const alunos = [
+	{
+		nome: 'João',
+		nota: 5,
+		turma: '1B',
+	},
+	{
+		nome: 'Sofia',
+		nota: 9,
+		turma: '1B',
+	},
+	{
+		nome: 'Paulo Paulinio',
+		nota: 6,
+		turma: '2C',
+	},
+    {
+		nome: 'Paulo Repetiu',
+		nota: 0,
+		turma: '2C',
+	},
+];
+
+function alunosAprovados(alunos, media) {
+	let aprovados = [];
+
+	for (let i = 0; i < alunos.length; i++) {
+		let { nota, nome } = alunos[i];
+
+		if (nota >= media) {
+			aprovados.push(nome);
+		}
+	}
+
+	return aprovados;
+}
+
+console.log(alunosAprovados(alunos, 5));
