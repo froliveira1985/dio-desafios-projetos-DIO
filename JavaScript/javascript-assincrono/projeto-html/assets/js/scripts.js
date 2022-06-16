34f002dbed59fa73a15ace225bc978a394afeca5
froliveira1985 <froliveira1985@gmail.com>
2022-06-16 17:36:08 -0300

    Atualizações JavaScript 160622

---
+++ JavaScript/javascript-assincrono/projeto-html/assets/js/scripts.js
@@ -0,0 +1,22 @@
+const BASE_URL = 'https://api.thecatapi.com/v1/images/search';
+const catBtn = document.getElementById('change-cat');
+
+const getCats = async () => {
+	try {
+		const data = await fetch(BASE_URL);
+		const json = await data.json();
+		return json[0].url;
+	} catch (e) {
+		console.log(e.message);
+	}
+};
+
+const loadImg = async () => {
+	const img = document.getElementsByTagName('img')[0];
+	img.src = await getCats();
+};
+
+
+catBtn.addEventListener('click', loadImg);
+
+loadImg();
---
+++ JavaScript/modulos/atividade/main.mjs
@@ -0,0 +1,7 @@
+import { mostraIdade, mostraCidade, mostraHobby } from './funcoes.mjs';
+
+console.log(mostraIdade('Fábio', 36));
+
+console.log(mostraCidade('Fábio', 'São Paulo'));
+
+console.log(mostraHobby('Fábio Oliveira', 'Ouvir música'));
