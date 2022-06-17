cb72f03ee0c764cf851283f4c0bf578bd399b5ee
froliveira1985 <froliveira1985@gmail.com>
2022-06-17 15:01:28 -0300

    TypeScript

---
+++ Pratica TypeScript/app.ts
@@ -0,0 +1,22 @@
+let button = document.getElementById('button');
+let input1 = document.getElementById('input1') as HTMLInputElement;
+let input2 = document.getElementById('input2') as HTMLInputElement;
+
+function adicionarNumero(numero1: number, numero2: number, devPrintar: boolean, frase: string) {
+    let resultado = numero1 + numero2
+    if (devPrintar) {
+        console.log(frase + resultado)
+    }
+    return numero1 + numero2
+}
+
+let devPrintar = true;
+let frase = 'O valor é: '
+
+if (button) {
+    button.addEventListener('click', () => {
+        if (input1 && input2) {
+            console.log(adicionarNumero(Number(input1.value), Number(input2.value), devPrintar, frase));
+        }
+    })
+}
