442acf826bf2217779006827dbdb7c46be7c5fbe
froliveira1985 <froliveira1985@gmail.com>
2022-07-22 17:34:24 -0300

    Springboot

---
+++ primeiros-passo/primeiros-passo/src/main/java/dio/springboot/PrimeirosPassoApplication.java
@@ -0,0 +1,13 @@
+package dio.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeirosPassoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeirosPassoApplication.class, args);
+	}
+
+}
---
+++ sprintboot/sprintboot/src/main/java/com/dio/sprintboot/SprintbootApplication.java
@@ -0,0 +1,13 @@
+package com.dio.sprintboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintbootApplication.class, args);
+	}
+
+}
